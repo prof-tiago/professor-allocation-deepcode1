cff9b9e4d91b24160b57eec007f698a330c9a0a2
Tiago Santos <professor.tiago.dos.santos@gmail.com>
2022-02-16 20:41:26 -0300

    Add project

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationClassApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/ProfessorAllocationClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
