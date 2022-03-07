f181d8324210b6174290f5495e9d56539a574fa7
Tiago Santos <professor.tiago.dos.santos@gmail.com>
2022-03-07 20:48:49 -0300

    Add methods to allocation controller

--- src/main/java/com/project/professor/allocation/controller/AllocationController.java
+++ src/main/java/com/project/professor/allocation/controller/AllocationController.java
@@ -1,7 +1,19 @@
 package com.project.professor.allocation.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.professor.allocation.entity.Allocation;
 import com.project.professor.allocation.service.AllocationService;
 
 @RestController
@@ -12,5 +24,60 @@ public class AllocationController {
 	public AllocationController(AllocationService allocationService) {
 		super();
 		this.allocationService = allocationService;
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping(path = "/allocations", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Allocation>> findAll() {
+		List<Allocation> allocations = allocationService.findAll();
+		return new ResponseEntity<>(allocations, HttpStatus.OK);
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping(path = "/allocations/{allocation_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Allocation> findById(@PathVariable(name = "allocation_id") Long id) {
+		Allocation allocation = allocationService.findById(id);
+		if (allocation != null) {
+			return new ResponseEntity<>(allocation, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(
+			path = "/allocations",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	public ResponseEntity<Allocation> create(@RequestBody Allocation allocation) {
+		try {
+			Allocation newAllocation = allocationService.create(allocation);
+			return new ResponseEntity<>(newAllocation, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@PutMapping(
+			path = "/allocations/{allocation_id}",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	public ResponseEntity<Allocation> update(@PathVariable(name = "allocation_id") Long id,
+			                                 @RequestBody Allocation allocation) {
+		allocation.setId(id);
+
+		try {
+			Allocation newAllocation = allocationService.update(allocation);
+			if (newAllocation != null) {
+				return new ResponseEntity<>(newAllocation, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 	}
 }
