ad101dd7b989ef05c0610ebe24609ad5ed327fea
Tiago Santos <professor.tiago.dos.santos@gmail.com>
2022-03-07 18:03:49 -0300

    Add controller classes

---
+++ src/main/java/com/project/professor/allocation/controller/DepartmentController.java
@@ -0,0 +1,16 @@
+package com.project.professor.allocation.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.professor.allocation.service.DepartmentService;
+
+@RestController
+public class DepartmentController {
+
+	private final DepartmentService departmentService;
+
+	public DepartmentController(DepartmentService departmentService) {
+		super();
+		this.departmentService = departmentService;
+	}
+}
