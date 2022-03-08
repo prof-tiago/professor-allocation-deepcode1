ae813ffc47ba5a99b429747b67b6ac190d986a34
Tiago Santos <professor.tiago.dos.santos@gmail.com>
2022-03-07 22:00:33 -0300

    Add findAll

--- src/main/java/com/project/professor/allocation/controller/DepartmentController.java
+++ src/main/java/com/project/professor/allocation/controller/DepartmentController.java
@@ -1,7 +1,16 @@
 package com.project.professor.allocation.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.professor.allocation.entity.Department;
 import com.project.professor.allocation.service.DepartmentService;
 
 @RestController
@@ -12,5 +21,12 @@ public class DepartmentController {
 	public DepartmentController(DepartmentService departmentService) {
 		super();
 		this.departmentService = departmentService;
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping(path = "/departments", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Department>> findAll(@RequestParam(name = "name", required = false) String name) {
+		List<Department> departments = departmentService.findAll(name);
+		return new ResponseEntity<>(departments, HttpStatus.OK);
 	}
 }
